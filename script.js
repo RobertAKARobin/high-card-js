44918dfff6c371600570e58767c3253d7070190c
Bipin Neupane <icomp2@hotmail.com>
2016-02-17 18:43:33 -0500

    write code for building deck

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 var values  = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
 var suits   = ["Clubs", "Diamonds", "Hearts", "Spades"];
 var game = {
-  deck: [values, suits],
+  deck: [(values.length * suits.length)],
   players: [a, b],
   hands: [1, 2],
+
   buildDeck: function(){
+    for (var i = 0; i < values.length; i++){
+      for (var i = 0; i < suits.length; i++)
+      console.log(deck [i]);
+    }
+  }
+
 
 /* build the deck by combining items in var values and var suits*/
 
   },
-  shuffleDeck: function(){
-    var shuffle = math.random(deck)
+  /* shuffleDeck: function() {
+    var shuffle = math.random(deck)} */
 
-/*shuffle deck by randomizing deck key inside var game*/
-  },
+/*shuffle deck by randomizing deck key inside var game
+  }, */
   getMorePlayers: function(){
 
   },
