a0de9f923f8ae5a3d8825fbceda457fcd1a97d17
Bipin Neupane <icomp2@hotmail.com>
2016-02-17 18:14:30 -0500

    pseudocode to understand

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 var values  = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
 var suits   = ["Clubs", "Diamonds", "Hearts", "Spades"];
 var game = {
-  deck: [],
-  players: [],
-  hands: [],
+  deck: [values, suits],
+  players: [a, b],
+  hands: [1, 2],
   buildDeck: function(){
+
+/* build the deck by combining items in var values and var suits*/
 
   },
   shuffleDeck: function(){
+    var shuffle = math.random(deck)
 
+/*shuffle deck by randomizing deck key inside var game*/
   },
   getMorePlayers: function(){
 
