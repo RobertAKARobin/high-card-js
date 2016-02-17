51a3e882f2146922c6f15d6d7776adc97b777e25
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-02-17 18:18:07 -0500

    initial commit, built deck, shuffled cards, asks for additional player

--- script.js
+++ script.js
@@ -4,15 +4,49 @@ var game = {
   deck: [],
   players: [],
   hands: [],
+
   buildDeck: function(){
+    for(var s = 0; s < suits.length; s++)
+    {for (var v = 0; v < values.length; v++)
+    {game.deck.push([values[v], suits[s]])}
+      }
+  }
+}
+  game.buildDeck.call();
 
-  },
-  shuffleDeck: function(){
+  function shuffleDeck(array) {
+    //Fisher Yates example used below
+    var m = array.length, t, i;
 
-  },
-  getMorePlayers: function(){
+      // While there remain elements to shuffle…
+      while (m) {
 
-  },
+        // Pick a remaining element…
+        i = Math.floor(Math.random() * m--);
+
+        // And swap it with the current element.
+        t = array[m];
+        array[m] = array[i];
+        array[i] = t;
+      }
+
+      return array;
+};
+
+shuffleDeck(game.deck);
+
+
+  function getMorePlayers() {
+    var askMorePlayers = prompt('Would you like to add one more player? Y / N').toUpperCase()
+    //if user asks to add another player, update game.players array with two indeces
+    if (askMorePlayers == 'Y') {game.players.push(0,1)}
+    //otherwise only send one index to game.players
+    else {game.players.push(0)}
+  }
+  //run function above
+  getMorePlayers()
+
+  /*
   deal: function(){
 
   },
@@ -26,3 +60,4 @@ var game = {
 
   }
 }
+*/
