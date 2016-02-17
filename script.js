0a7b7163916db6bb8d9ec00814d1f8e651dd1193
Patrick Moran <patrick_moran@mac.com>
2016-02-17 14:03:50 -0500

    initial commit + buildDeck and shuffleDeck functions

--- script.js
+++ script.js
@@ -5,9 +5,29 @@ var game = {
   players: [],
   hands: [],
   buildDeck: function(){
-
+    for (var value = 0; value < values.length; value++) {
+      for (var suit = 0; suit < suits.length; suit++) {
+        this.deck.push([values[value], suits[suit]]);
+      }
+    }
+    // console.log(this.deck);
   },
   shuffleDeck: function(){
+    // Fisher-Yates shuffle!
+    var hold, randomIndex;
+    // loop through the deck in reverse order
+    for (var i = this.deck.length - 1; i > 0; i--) {
+      // generate a random index -> Round down((random decimal * number of remaining cards))
+      randomIndex = Math.floor(Math.random() * (i + 1));
+      // set the current loop's value to the variable "hold"
+      hold = this.deck[i];
+      // swap the current loop's value with the value at the random index
+      this.deck[i] = this.deck[randomIndex];
+      // replace the value at random index with the current loops value
+      this.deck[randomIndex] = hold;
+      // repeat until no values remain to be shuffled
+    }
+    // console.log(this.deck);
 
   },
   getMorePlayers: function(){
