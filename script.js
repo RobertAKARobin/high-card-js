7525f8e1f62068b9b0b56d37f88e6f6332d3ffc8
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-02-17 18:46:40 -0500

    dealing is up for two players

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ var game = {
   game.buildDeck.call();
 
   function shuffleDeck(array) {
-    //Fisher Yates example used below
+    //Fisher Yates example used below, looked up online
     var m = array.length, t, i;
 
       // While there remain elements to shuffle…
@@ -38,18 +38,25 @@ shuffleDeck(game.deck);
 
   function getMorePlayers() {
     var askMorePlayers = prompt('Would you like to add one more player? Y / N').toUpperCase()
-    //if user asks to add another player, update game.players array with two indeces
-    if (askMorePlayers == 'Y') {game.players.push(0,1)}
-    //otherwise only send one index to game.players
-    else {game.players.push(0)}
+    //if user asks to add another player, update game.players array with three indeces
+    if (askMorePlayers == 'Y') {game.players.push(0,1,2)}
+    //otherwise only send two indeces for game, player and computer
+    else {game.players.push(0,1)}
   }
   //run function above
   getMorePlayers()
 
-  /*
-  deal: function(){
+//deal function works only if players are player and computer :(
+  function deal(){
+    for (var i = 1; i<=game.deck.length; i++)
+    { //push up shuffled even indexed cards to player hand
+      if (i%2===0) {game.hands.push("Player: "+game.deck[i])}
+      //otherwise push odd numbered index cards to computer hand
+    else {game.hands.push("Computer: "+game.deck[i])}}}
+  }
+}
 
-  },
+  /*
   findHighestCard: function(){
 
   },
