e8ac31928e0b1fb12be78005410c227b00ea50df
Nyron Curtis Waite <Nyroncurtiswaite@gmail.com>
2016-02-18 08:24:35 -0500

    new changes

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-var values  = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
-var suits   = ["Clubs", "Diamonds", "Hearts", "Spades"];
+var values = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+var suits  = ["Clubs", "Diamonds", "Hearts", "Spades"];
+var choices = values.concat(suits);
 var game = {
   deck: [],
   players: [],
   hands: [],
-  buildDeck: function(){
+  buildDeck: function(values , suits){
+
+game.deck = choices;
 
   },
+
   shuffleDeck: function(){
+
+  Math.Random();
 
   },
   getMorePlayers: function(){
@@ -25,4 +31,5 @@ var game = {
   playANewGame: function(){
 
   }
+
 }
