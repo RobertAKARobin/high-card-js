4caf03ed1fe197d698edeab1fe76d4706b00b51d
Mohamed Chalal <Mohamedrchalal@gmail.com>
2016-02-17 19:38:42 -0500

    very confused

--- script2.js
+++ script2.js
@@ -1,14 +1,25 @@
-
 var values  = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
 var suits   = ["Clubs", "Diamonds", "Hearts", "Spades"];
+
+
 var game = {
   deck: [],
   players: [],
   hands: [],
   buildDeck: function(){
-
-  },
+    for (i = 0;i <suits.length;i++){
+      for(j = 0;j<values.length;j++){
+        console.log(suits[i]+' '+values[j]);
+        this.deck.push(suits[i]+' '+values[j]);
+      }
+    }
+}
+};
+ console.log(game.deck.length);
   shuffleDeck: function(){
+
+  }
+  }
 
   },
   getMorePlayers: function(){
