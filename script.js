a66ea25c738540989bd7003540e9b2ecce515e15
winishasmith <winishasmith@gmail.com>
2016-02-18 08:53:22 -0500

    updated buildDeck and SshuffleDeck.

--- script.js
+++ script.js
@@ -1,28 +1,25 @@
 var values  = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
 var suits   = ["Clubs", "Diamonds", "Hearts", "Spades"];
-function card (value,suit ) {
-this.value = value;
-this.suit = suit;
-}
 var game = {
   deck: [],
   players: [],
   hands: [],
   buildDeck: function(){
-    this.values = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
-    this.suits = ["Clubs", "Diamonds", "Hearts", "Spades"];
+    values = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+    suits = ["Clubs", "Diamonds", "Hearts", "Spades"];
       var deck = [];
-        for( var v = 0; v < this.values.length; v++ ) {
-          for( var s = 0; s < this.suits.length; s++ ) {
-            deck.push( new card(this.values[v], this.suits[s]));
+        for( var v = 0; v < values.length; v++ ) {
+          for( var s = 0; s < suits.length; s++ ) {
+            this.deck.push(values[v], suits[s]);
           }
         }
-    return deck;
+    return this.deck;
   },
   shuffleDeck: function(){
-    deck.sort(function(a, b){
-      return a-b
+    this.deck.sort(function(a, b){
+      return a-b;
     });
+    return this.deck;
   },
   getMorePlayers: function(){
 
