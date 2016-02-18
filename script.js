34c88dc0409c3cd7d9b4101de560ad60d2070110
ALEXA KLEIN <lexi.klein1@gmail.com>
2016-02-17 23:39:32 -0500

    worked on it for a while but not understanding

--- script.js
+++ script.js
@@ -5,24 +5,33 @@ var game = {
   players: [],
   hands: [],
   buildDeck: function(){
-
+    values.forEach(function(value) {
+      suits.forEach(funcion(suit) {
+        this.deck.push[(value,suit)];
+      } );
+    } );
   },
   shuffleDeck: function(){
-
+    deck.sort(function();
+  );
   },
   getMorePlayers: function(){
+    var playerName;
+    prompt("There are " + players.length + "players thus far. Do you want to add another player?");
+    players.push(playerName);
+    players.getMorePlayers();
 
   },
   deal: function(){
-
+    //give each player one card//
   },
   findHighestCard: function(){
-
+    //highest card between 2 and Ace//
   },
   announceWinners: function(){
 
   },
   playANewGame: function(){
-
+    //repeat all functions: buildDeck(), shuffleDeck(), getMorePlayers(), deal(), findHighestCard(), announceWinners(), playANewGame()//
   }
 }
