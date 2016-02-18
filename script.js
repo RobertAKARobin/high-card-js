4956c1cb266d4955cef308bedf18e2f725e53ddd
Keith Prifte <kprift@gmail.com>
2016-02-17 19:18:30 -0500

    adds buildDeck, shuffleDeck, getMorePlayers, and deal functions

--- script.js
+++ script.js
@@ -5,16 +5,26 @@ var game = {
   players: [],
   hands: [],
   buildDeck: function(){
+    for (i = 0; i < values.length; i += 1) {
+        for (j = 0; j < suits.length; j += 1)
+    game.deck.push(values[i] + " of " + suits[j]);
+    }
 
   },
   shuffleDeck: function(){
-
+    game.deck.sort();
   },
   getMorePlayers: function(){
+    game.players.push(prompt('Would you like to add another player to the game?  If so, type their name.'))
 
   },
   deal: function(){
-
+    //for (i = 0; i < game.players.length; i += 1) {
+    //hands.push.(players[i] +
+    var card = game.deck[parseInt(Math.random() * game.deck.length)]
+    console.log(card)
+    game.hands.push({playerName: game.players[0], dealtCard: card})
+    
   },
   findHighestCard: function(){
 
