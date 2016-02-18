3f3aded08f2976006f92cbad4fd7762b95260925
Melissa Miller <lis.d.miller@gmail.com>
2016-02-17 21:22:20 -0500

    Adds buildDeck and shuffleDeck methods

--- script.js
+++ script.js
@@ -5,11 +5,36 @@ var game = {
   players: [],
   hands: [],
   buildDeck: function(){
-
+    var self = this
+    values.forEach(function(value)
+    {
+      suits.forEach(function(suit)
+      {
+        self.deck.push([value, suit]);
+      });
+    });
   },
+
   shuffleDeck: function(){
+    // Using the Fisher-Yates (Knuth) shuffle
+    var currentIndex = this.deck.length
+      , temporaryValue
+      , randomIndex;
 
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = this.deck[currentIndex];
+      this.deck[currentIndex] = this.deck[randomIndex];
+      this.deck[randomIndex] = temporaryValue;
+    }
   },
+
   getMorePlayers: function(){
 
   },
