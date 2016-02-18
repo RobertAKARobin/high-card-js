1beeef0fdf1f45cfe97f38afaa4e62e2af532011
BrittanyBranson <brittanydibble@Brittanys-MacBook-Pro.local>
2016-02-18 00:02:57 -0500

    initial commit

--- script.js
+++ script.js
@@ -4,24 +4,43 @@ var game = {
   deck: [],
   players: [],
   hands: [],
+//Populates deck with standard cards
   buildDeck: function(){
-
+    for (var i = 0; i < values.length; i++){
+      for (var i = 0; i < suits.length; i++)
+      console.log(deck [i]);
+    }
   },
+//randomizes deck
   shuffleDeck: function(){
+    var newDeck = [];
+    for (var i = 0; i < deck.length; i++)
+      newDeck[i] = deck.splic(Math.floor(Math.random());
+//had to look this one up on the internet. I don't think it's complete. 
+
+    }
 
   },
+//confirms whether or not the player wants to add additional players
   getMorePlayers: function(){
+    var playerName = prompt("Enter Name of Additional Player?");
+    players.push([playerName]);
+  }
+},
 
-  },
+//gives each player a card
   deal: function(){
 
   },
+//finds the player with the card of the highest value
   findHighestCard: function(){
 
   },
+//player's name, card, and rank
   announceWinners: function(){
 
   },
+//runs entire game
   playANewGame: function(){
 
   }
