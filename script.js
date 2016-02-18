11d11fd127f4642073a2abefc2907c66ae392e71
Clarissa Stark <clarissa.stark@gmail.com>
2016-02-18 02:27:12 -0500

    made initial attempt to think through each function

--- script.js
+++ script.js
@@ -4,24 +4,58 @@ var game = {
   deck: [],
   players: [],
   hands: [],
+
+// This is getting the value of each card and the suit of each card for 52 cards
+
   buildDeck: function(){
-
+    for(var s = 0; i < suits.length; s++) {
+      for(var v = 0; values.lenth; v++) {
+        game.deck.push(values[v], suits[s]);
+      }
+    }
+    alert("The deck has been built.");
+    return game.deck;
   },
+
+// This takes the deck array after it's built and reorders it
+
   shuffleDeck: function(){
-
+    new game.deck.sort;
   },
-  getMorePlayers: function(){
 
-  },
+// This pushes a new player or players to the array
+
+  getMorePlayers: function(player){
+    addPlayer = confirm("Would you like to add a player?");
+      if (addPlayer == true) {
+      this.players.push(player);
+      }
+      else {
+      alert("Ok, no new players will be added.");
+      }
+    },
+
+// This takes the deck array and randomly assigns one card to each player
+
   deal: function(){
-
+    self = this;
+    for (var p  = 0; p < self.players.length; p++)
   },
+
+// This finds the highest card in the hand arrays for each player
+
   findHighestCard: function(){
-
+    for this.player
   },
+
+// This uses the findHighestCard function to determine which player in the array is the winner
+
   announceWinners: function(){
 
   },
+
+// This clears the console to play a new game
+
   playANewGame: function(){
 
   }
