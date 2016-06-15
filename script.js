5374f98fddc092850128236851669ac1e74c3e61
Anh Truong <anh.truong.07@cnu.edu>
2016-06-15 19:41:05 -0400

    finished

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var values  = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+var values  = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"];
 var suits   = ["Clubs", "Diamonds", "Hearts", "Spades"];
 var game = {
   deck: [],
@@ -28,21 +28,21 @@ var game = {
   },
   getMorePlayers: function(){
     var addPlayers = true;
-    while (addPlayers === true){
-    var newPlayer = prompt("Would you like to add a player? Y/N")
-      if (newPlayer === "Y"){
-          var playerName = prompt("What is your name?")
-          game.players.push({name: playerName, hand:[]})
-      }
-      if (newPlayer === "N"){
-        return addplayers = false;
-      }
+    while (addPlayers === true){ //loop to keep adding players then adds player count, stops with a N
+      var newPlayer = prompt("Would you like to add a player? Y/N")
+        if (newPlayer === "Y"){
+            var playerName = prompt("What is your name?")
+            game.players.push({name: playerName, hand:[]})
+        }
+        if (newPlayer === "N"){
+          return addplayers = false;
+        }
       }
   },
   deal: function(){
     for (var i=0; i<game.players.length;i++){
       var dealtCard = game.deck.shift();
-      game.players[i].hand.push(dealtCard);
+      game.players[i].hand.push(dealtCard); //using the principle that the array and objects are linked together, this arranges the first object in the object 'hand' which is hte cardRank.
     }
 
   },
@@ -51,14 +51,28 @@ var game = {
       return b.hand[0].cardRank-a.hand[0].cardRank});
   },
   announceWinners: function(){
-
+    //game.players[0].hand[0].suit
+    //game.players[0].hand[0].value
+    for (var i=0; i < game.players.length; i++){
+      console.log("Rank"+(i+1)+" is "+game.players[i].name+" with the "+game.players[i].hand[0].value+" of "+game.players[i].hand[0].suit+"!")
+   }
   },
   playANewGame: function(){
-
-  }
+    var playAgain = prompt("Rematch? Y/N")
+      if (playAgain === "Y"){
+        game.players = [];
+        play();
+      }
+    }
 }
 game.buildDeck();
-game.shuffleDeck(game.deck);
-game.getMorePlayers();
-game.deal();
-game.findHighestCard();
+
+function play(){
+  game.shuffleDeck(game.deck);
+  game.getMorePlayers();
+  game.deal();
+  game.findHighestCard();
+  game.announceWinners();
+  game.playANewGame();
+}
+play();
