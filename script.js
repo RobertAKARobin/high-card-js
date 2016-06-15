2d64151ff80224027c9f868f86508441255587d8
Kevin Golden <kwg@cs.net>
2016-06-15 13:30:39 -0400

    Wednesday morning

--- script.js
+++ script.js
@@ -4,32 +4,54 @@ var game = {
   deck: [],
   players: [],
   hands: [],
+  power: [],
   buildDeck: function(){
-  // loop through the values array. For every value in values, loop through
-  // the suits array.  Combine the two values to get 2C, 2H, 4D, etc. and
-  // put each element in the value array.
-value.forEach()
+    // loop through the values array. For every value in values, loop through
+    // the suits array.  Combine the two values to get 2C, 2H, 4D, etc. and
+    // put each element in the value array.
+    var pValue = 52;
+    for (i = values.length - 1; i >= 0; i--) {
+      for (j = suits.length - 1; j >= 0; j--) {
+        var outVar = values[i].toString() + suits[j].substring(0,1);
+        cards.unshift(outVar);
+        pValues.unshift(pValue);
+        pValue-=1;
+      }
+    }
   },
   shuffleDeck: function(){
-// build an array of 52 random numbers.
-//
-// end up with an array named Value with the shuffled cards.
+    // build an array of 52 random numbers.
+    // fruits.splice(n, 1);
+    // end up with an array containing pointers with the shuffled cards.
+    // n=52
+    // do n = 52 to 0
+    // pick a random number between 1 and n --> x
+    // j = splice element x from the cards array
+    // push j to shuffled
+    // loop
+    shuffled = [];
+    for (i=52; i>0; i--) {
+      pickIndex = Math.floor((Math.random() * i) + 1
+
+    }
+
+    Math.floor((Math.random() * 10) + 1
   },
   getMorePlayers: function(){
-//prompt for player names, create empty arrays for each name
+    //prompt for player names, 
   },
   deal: function(){
-// Each trick involves two pops, one for player A and one for player B.
-// create an Trick array that has two cards, one for each player
-// End with a loop that runs through the deck and execute each trick.
+    // Each trick involves two pops, one for player A and one for player B.
+    // create an Trick array that has two cards, one for each player
+    // End with a loop that runs through the deck and execute each trick.
   },
   findHighestCard: function(){
-// sort the Trick array and return the highest card and winner name
+    // sort the Trick array and return the highest card and winner name
   },
   announceWinners: function(){
     // call findHighestCard to determine the winner
   },
   playANewGame: function(){
-// reset the arrays, get players, shuffle, and deal
+    // reset the arrays, get players, shuffle, and deal
   }
 }
--- test.js
+++ test.js
@@ -1,16 +1,57 @@
-var values  = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
-var suits   = ["Clubs", "Diamonds", "Hearts", "Spades"];
-var cards = [];
-var pValues = [];
+
+// function getPlayers() {  // does not handle null response
+//   // var person1 = "";
+//   // var person2 = "";
+//   var person1 = prompt("Enter the first user name.\nJust hit enter to let the computer pick names.", "Name");
+//   if (person1 == ("Name" || "" || null)) {
+//     var person1 = "Computer 1";
+//     var person2 = "Computer 2";
+//   } else {
+//     var person2 = prompt("Enter the second user name.\nJust hit enter to let the computer pick.", "Name");
+//     if (person2 == ("Name" || "" || null)) {
+//       var person2 = "Computer 2";
+//     }
+//   }
+//
+//
+//   console.log("Player 1: " + person1);
+//   console.log("Player 2: " + person2);
+// }
+function shuffleDeck() {
+  cards = ["2C", "2D", "2H", "2S", "3C", "3D", "3H", "3S", "4C", "4D", "4H", "4S", "5C", "5D", "5H", "5S", "6C", "6D", "6H", "6S", "7C", "7D", "7H", "7S", "8C", "8D", "8H", "8S", "9C", "9D", "9H", "9S", "10C", "10D", "10H", "10S", "JC", "JD", "JH", "JS", "QC", "QD", "QH", "QS", "KC", "KD", "KH", "KS", "AC", "AD", "AH", "AS"]
+  shuffled = [];
+  for (i=51; i>=0; i--) {
+    // pickIndex = Math.floor((Math.random() * i) + 1)
+    pickIndex = Math.floor((Math.random() * i));
+    xxx = cards.splice(pickIndex,1);
+    shuffled.push(xxx.toString());
+    // console.log(i + "  index: " + pickIndex);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
 function buildDeck() {
-  var pValue = 52;
+  var values  = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+  var suits   = ["Clubs", "Diamonds", "Hearts", "Spades"];
+  var cards = [];
+  var cardValues = [];
+  var cardValue = 52;
   for (i = values.length - 1; i >= 0; i--) {
     for (j = suits.length - 1; j >= 0; j--) {
       var outVar = values[i].toString() + suits[j].substring(0,1);
       // console.log("outVar: " + outVar);
       cards.unshift(outVar);
-      pValues.unshift(pValue);
-      pValue-=1;
+      cardValues.unshift(cardValue);
+      cardValue-=1;
 
       // console.log(i + "  value: " + values[i]);
       // console.log(j + "  suit: " + suits[j]);
