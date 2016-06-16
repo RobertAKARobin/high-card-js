260c382fe4ff7d8ec765a9fca55199af9cf35d06
James Hughes <heezmagnif@gmail.com>
2016-06-16 09:00:12 -0400

    Initial commit

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
 var values  = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
 var suits   = ["Clubs", "Diamonds", "Hearts", "Spades"];
 var game = {
-  deck: [],
+  deck: [];
   players: [],
   hands: [],
   buildDeck: function(){
-
+    for (i=0, i < values, i++){
+      for (j=0, j < suits, j++){
+        this.deck.push([values[i],suits[j]]);
+      }
+    }
   },
   shuffleDeck: function(){
+    function shuffle(array) {
+      var currentIndex = array.length, temporaryValue, randomIndex;
 
+      
+      while (0 !== currentIndex) {
+
+
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+      }
+
+      return array;
+    }
   },
   getMorePlayers: function(){
 
