d524fc68d108f283079136f3ba4e1fa1dacd0fdb
Mara Sanders <sandersmara@gmail.com>
2016-06-16 11:00:43 -0400

    Finished

--- script.js
+++ script.js
@@ -8,15 +8,14 @@ var game = {
   buildDeck: function(){
     for (var i=0; i<13; i++) {
       //create cards 1-13 suit Clubs
-      game.deck[i] = {value:values[i],suit:suits[0],points:i};
+      game.deck[i] = {value:values[i],suit:suits[0],points:i,playerID:0};
       //create cards 1-13 suit Diamonds
-      game.deck[i+13] = {value:values[i],suit:suits[1],points:i};
+      game.deck[i+13] = {value:values[i],suit:suits[1],points:i,playerID:0};
       //create cards 1-13 suit Hearts
-      game.deck[i+26] = {value:values[i],suit:suits[2],points:i}
+      game.deck[i+26] = {value:values[i],suit:suits[2],points:i,playerID:0}
       //create cards 1-13 suit Spades
-      game.deck[i+39] = {value:values[i],suit:suits[3],points:i}
+      game.deck[i+39] = {value:values[i],suit:suits[3],points:i,playerID:0}
     }
-    console.log (game.deck)
   },
   shuffleDeck: function(){
   var cardToShuffle = game.deck.length
@@ -32,7 +31,6 @@ var game = {
     game.deck[randomCard] = tempHold;
 
   }
-  console.log (game.deck)
   },
   getMorePlayers: function(){
     for (var i=0; i<6; i++) {
@@ -40,35 +38,43 @@ var game = {
      if (!tempName){
        return;
      }
-     game.players[i] = {name:tempName}
-     console.log(game.players)
-     console.log(i)
+     game.players[i] = tempName
+     if (i == 5){
+        console.log("Maximum of six players has been reached")
+     }
    }
   },
   deal: function(){
+    l = game.deck.length
+    for (var l = 0; l < game.deck.length;l=l+game.players.length){
     game.hands = []
-    if (game.deck.length < game.players.length){
+    if ((game.deck.length-l) < game.players.length){
       console.log("Game Over Good Game!")
       return
     }
     for (var i=0; i<game.players.length; i++) {
       //take the first card and give it to player i
-        game.hands [i] =  game.deck.shift();
+        game.hands[i] =  game.deck.shift();
   // player i has his card return it to the bottom of the deck before adding name
         game.deck.push(game.hands[i])
   // this should add the right player to the hand before we sort it by points
-        game.hands.push.apply(game.hands[i],game.players[i]);
+        game.hands[i].playerID = i
     }
     game.hands.sort(function(a, b){
-      return a.points-b.points
+      return b.points-a.points
     })
-    for (var i=(game.hands.length-1); i<0; i=i-1) {
-  //    console.log(game.players[i]+" is number "+(i+1)"with the "+game.hand.value+" "+game.hand.suit+"!")
+    for (var i=0; i<(game.hands.length); i++) {
+      var j = i+1
+      var k = game.hands[i].playerID
+     console.log(game.players[k]+" is number "+j+" with the "+game.hands[i].value+" of "+game.hands[i].suit+"!")
     }
+}
 }
 };
 game.buildDeck();
 game.shuffleDeck();
 game.getMorePlayers();
+if (game.players.length<6){
 console.log (game.players.length+" Players will Play the Game");
+};
 game.deal();
