5b9235d4b5f89e9e3112ba36e071b46a21318980
Kevin Golden <kwg@cs.net>
2016-06-15 22:15:21 -0400

    Looks good - push this mother

--- script.js
+++ script.js
@@ -4,35 +4,32 @@ window.onload = function() {
 game.playANewGame();
 }
 
+var values  = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+var suits   = ["Clubs", "Diamonds", "Hearts", "Spades"];
+
 var game = {
-  deck: [],        // contains a deck of cards
+  deck: [],       // contains a deck of cards
   players: [],     // contains player names
   hands: [],       // contains the values of the cards dealt in a hand
   cardValues: [],  // contains the values of the deck of cards, 1-52
   shuffledValues: [],   // shuffled version of cardValues
   deck: [],        // contains a deck of cards
-  players: [],     // contains player names
+  players: ["Computer 1", "Computer 2"],     // contains player names
   hands: [],       // contains the values of the cards dealt in a hand
   cardValues: [],  // contains the values of the deck of cards, 1-52
   shuffledValues: [],   // shuffled version of cardValues
+  wins0: 0,
+  wins1: 1,
   buildDeck: function(){
     console.log("buildDeck");
-    var values  = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
-    var suits   = ["Clubs", "Diamonds", "Hearts", "Spades"];
-    var cards = [];
-    var cardValues = [];
     var cardValue = 52;
     for (i = values.length - 1; i >= 0; i--) {
       for (j = suits.length - 1; j >= 0; j--) {
-        var outVar = values[i].toString() + suits[j].substring(0,1);
+        outVar = values[i].toString() + suits[j].substring(0,1);
         // console.log("outVar: " + outVar);
-        cards.unshift(outVar);
-        cardValues.unshift(cardValue);
+        game.deck.unshift(outVar);
+        game.cardValues.unshift(cardValue);
         cardValue-=1;
-
-        console.log(i + "  value: " + values[i]);
-        console.log(j + "  suit: " + suits[j]);
-
       }
     }
   },
@@ -48,69 +45,58 @@ var game = {
   },
   shuffleDeck: function(){
     console.log("shuffleDeck");
-    // console.log("shuffleDeck");
-    // shuffles the cardValues array.
-    shuffledValues = game.shuffleArray(game.cardValues);
+    game.shuffledValues = game.shuffleArray(game.cardValues);
   },
-  getMorePlayers: function(){
+  getMorePlayers: function(){  // change the default values in the array if the user enters anything.
     console.log("Get more players");
     var input = prompt("Enter the first user name.\nJust hit enter to let the computer pick names.\nHit Cancel if you don't want to play", "Name");
     if (input) {
       if (input !== "Name") {
-        player1 = input;
-        var input = prompt("Enter the second user name.\nJust hit enter to let the computer pick.", "Name");
-        if (input) {
-          if (input !== "Name") {
-            player2 = input;
-          } // user just hit enter at second prompt: one computer user
-          else  {
-            player2 = "Computer";
-          }
-        }  // user cancelled second prompt: one computer user
-        else  {
-          player2 = "Computer";
-        }
-      } else if (input == "Name" || input == "") { // user just hit Enter at first prompt:  two computer users
-        player1 = "Computer 1";
-        player2 = "Computer 2";
+        game.players.splice(0, 1, input);
+      }
     }
-    else { //user hit cancel at first prompt
-      player1 = "cancel";
-    }}
-    console.log("Player 1: " + player1);
-    console.log("Player 2: " + player2);
-    //prompt for player names,
+    var input = prompt("Enter the second user name.\nJust hit enter to let the computer pick.", "Name");
+    if (input) {
+      if (input !== "Name") {
+        game.players.splice(1, 1, input);
+      }
+    }
   },
   deal: function(){
     console.log("deal");
     // use splice to get two cards from the shuffledValues array
     // and put them in the hands array.
     // End with a loop that runs through the deck and execute each trick.
-    var hands = [];
-    hands = shuffledValues.splice(0,2);
-    // var winner = (hands[0] > hands[1]) ? players[0]:players[1];
-    if (hands[0] > hands[1]) {
-      winner = players[0];
-      wins0 ++;
+    game.hands = game.shuffledValues.splice(0,2);
+    if (game.hands[0] > game.hands[1]) {
+      winner = game.players[0];
+      game.wins0 ++;
     } else {
-      winner = players[1];
-      wins1 ++;
+      winner = game.players[1];
+      game.wins1 ++;
     }
-    line1 = players[0] + " draws: " + cards[hands[0]-1] + ", " + players[1] + " draws: " + cards[hands[1]-1];
+    line1 = game.players[0] + " draws: " + game.deck[game.hands[0]-1] + ", " + game.players[1] + " draws: " + game.deck[game.hands[1]-1];
     line2 = winner + "  wins!";
-    line3 = "Score: " + players[0] + ": " + wins0 + ", " + players[1] + ": " + wins1;
+    line3 = "Score: " + game.players[0] + ": " + game.wins0 + ", " + game.players[1] + ": " + game.wins1;
     alert(line1 + "\n" + line2 + "\n" + line3 );
-    if (shuffledValues.length == 0) {
-      console.log("Game Over");
-    }
   },
   findHighestCard: function(){
-    // sort the Trick array and return the highest card and winner name
     // done in the deal routine
   },
   announceWinners: function(){
-    // call findHighestCard to determine the winner
-    // done in the deal routine
+    var gameWinner = "";
+    line1 = "Game Over!";
+    if (game.wins0 > game.wins1) {
+      gameWinner = players[0];
+      var tricksWinner = game.wins0;
+      var tricksLoser = game.wins1;
+    } else {
+      gameWinner = game.players[1];
+      tricksWinner = game.wins1;
+      tricksLoser = game.wins0;
+    }
+    line2 = gameWinner + " wins by a score of " + tricksWinner + " to " + tricksLoser;
+    alert (line1 + "\n" + line2);
   },
   playANewGame: function(){
     // reset the arrays, get players, shuffle, and deal
@@ -118,6 +104,11 @@ var game = {
     game.getMorePlayers();
     game.buildDeck();
     game.shuffleDeck();
+    while (game.shuffledValues.length > 0) {
+      game.deal()
+      // console.log("Game Over");
+    }
+    game.announceWinners();
 
   }
 }
